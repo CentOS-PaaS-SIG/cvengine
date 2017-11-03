ee65c44ed370efb9bc6d2ddfb239c234420583ad
Alex Corvin <acorvin@redhat.com>
2017-11-03 15:23:35 -0400

    Add a setup script to install the module

---
+++ setup.py
@@ -0,0 +1,13 @@
+#! /usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='cvengine',
+      version='1.0',
+      description='Red Hat container validation engine',
+      author='Alex Corvin',
+      author_email='acorvin@redhat.com',
+      packages=['cvengine'],
+      install_requires=['paramiko',
+                        'scp',
+                        'diaper'])
