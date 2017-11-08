215fef14ad63b4b4f023b6f6b9862a3aee8da1e2
Alex Corvin <acorvin@redhat.com>
2017-11-08 15:41:51 -0500

    Increment package version to 1.1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name='cvengine',
-      version='1.0',
+      version='1.1',
       description='Red Hat container validation engine',
       author='Alex Corvin',
       author_email='acorvin@redhat.com',
