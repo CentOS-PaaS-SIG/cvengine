a1a2ac74582eeb299792dfa18885859c9dbdd3b6
Alex Corvin <acorvin@redhat.com>
2017-11-28 18:28:14 -0500

    Add PreConfiguredEnvironment handler
    The preconfigured environment is for when the container platform has
    already been deployed and we just need to know the connection
    information. This is how all container validations run today, so it
    needs to be the first (and default) environment type. This implements
    this envionment handler and updates the platform handlers to make use of
    environments.

--- cvengine/platform_handlers/atomic_host_handler.py
+++ cvengine/platform_handlers/atomic_host_handler.py
@@ -12,22 +12,20 @@ class AtomicHostHandler(BasePlatformHandler):
     on the Atomic host.
 
     """
-    def __init__(self, host_test, env_config_yaml,
+    def __init__(self, host_test, environment,
                  artifacts, common_vars):
 
-        super(AtomicHostHandler, self).__init__(host_test, env_config_yaml,
+        super(AtomicHostHandler, self).__init__(host_test, environment,
                                                 artifacts, common_vars)
 
         self.extra_vars['exec_cmd'] = 'docker {0}'.format(self.EXEC_CMD_SUFFIX)
         self.fetch_artifact_cmd = 'docker cp'
 
-        self.test_host = self.env_config['atomic-host'][0]
-        self.remote_host = self.test_host['ip_address']
-        credentials = self.test_host['credentials']
-        user = credentials['user']
-        key_path = credentials.get('ssh_key_path', None)
-        password = credentials.get('password', None)
-        port = credentials.get('port', 22)
+        self.remote_host = environment.host_ip
+        user = environment.username
+        key_path = environment.ssh_key_path
+        password = environment.password
+        port = environment.port
         if not key_path and not password:
             msg = 'Either a private key path or password must be given'
             raise ValueError(msg)
@@ -39,8 +37,8 @@ class AtomicHostHandler(BasePlatformHandler):
             'port': port
         }
 
-        self.extra_vars['current_host_ip'] = self.test_host['ip_address']
-        self.extra_vars['host_machine_name'] = self.test_host['machine_name']
+        self.extra_vars['current_host_ip'] = self.remote_host
+        self.extra_vars['host_machine_name'] = environment.host_name
         self.run_playbooks_locally = False
         self.ansible_cmd = ('ANSIBLE_CONFIG={cfg} '
                             'ansible-playbook '
